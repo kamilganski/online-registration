adbf076b14c39375c464a963de08b7caaac3b6e9
Kamil Ganski <kamilganski2@gmail.com>
2020-02-17 15:07:51 +0100

    Add visit table entity

---
+++ src/main/java/com/ganski/entity/Visit.java
@@ -0,0 +1,40 @@
+package com.ganski.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Time;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+public class Visit {
+
+    @Id
+    @Column(name = "Id")
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "Date")
+    private Date date;
+
+    @Column(name = "Time")
+    private Time time;
+
+    @Column(name = "Number")
+    private int number;
+
+    @OneToOne
+    @JoinColumn(name = "Doctor_Id")
+    private Doctor doctor;
+
+    @OneToOne
+    @JoinColumn(name = "User_Id")
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "Office_Id")
+    private Office office;
+}
